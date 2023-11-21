3d774045f4ef7853d94ee3fe4ac9214e84bd54e8
{psm4171} <{kkjjhh4171@naver.com}>
2023-11-21 09:39:55 +0900

    Add Exam03.java

---
+++ Network Programming/javaSocket/Exam03.java
@@ -0,0 +1,42 @@
+package javaSocket;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Exam03 {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 4171;
+
+        if (args.length > 0) {
+            host = args[0];
+        }
+
+        try {
+            if (args.length > 1) {
+                port = Integer.parseInt(args[1]);
+            }
+        } catch (NumberFormatException ignore) {
+            System.err.println("Port가 올바르지 않습니다.");
+            System.exit(1);
+        }
+
+        try {
+            Socket socket = new Socket(host, port);
+            System.out.println("서버에 연결되었습니다.");
+
+            int ch;
+            while ((ch = socket.getInputStream().read()) >= 0) {
+                System.out.write(ch);
+                if (ch < 0) {
+                    System.out.println("서버에서 연결을 종료했습니다.");
+                    break;
+                }
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
