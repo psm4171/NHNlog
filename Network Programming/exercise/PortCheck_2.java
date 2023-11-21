3000e19006b1abe7e1eb54140a9816f939e9b985
{psm4171} <{kkjjhh4171@naver.com}>
2023-11-21 09:17:38 +0900

    Port Checking exercies.

---
+++ Network Programming/exercise/PortCheck_2.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class PortCheck_2 {
+    public static void main(String[] args) {
+        int startPort = 0;
+        int endPort = 65535;
+
+        if (args.length < 2) {
+            System.err.println("검색할 포트 범위 지정이 필요합니다.");
+        }
+
+        try {
+            startPort = Integer.parseInt(args[1]);
+            endPort = Integer.parseInt(args[0]);
+
+            if (endPort < startPort) {
+                int temp = startPort;
+                startPort = endPort;
+                endPort = temp;
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("포트 올바르지 않습니다.");
+        }
+
+        for (int port = startPort; port < endPort; port++) {
+            // try-with-resources
+            try (Socket socket = new Socket("localhost", port)) {
+                System.out.println("Port " + port + " 열려 있습니다.");
+                socket.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+}
