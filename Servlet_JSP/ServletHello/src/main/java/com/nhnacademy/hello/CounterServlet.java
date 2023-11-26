e10e93b7b29f77db2c013cf5023e8733854321d5
SeungMin Park <94738749+psm4171@users.noreply.github.com>
2023-11-26 15:42:30 +0900

    Create CounterServlet.java

---
+++ Servlet_JSP/ServletHello/src/main/java/com/nhnacademy/hello/CounterServlet.java
@@ -0,0 +1,44 @@
+package com.nhnacademy.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
+
+public class CounterServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(CounterServlet.class.getName());
+
+    private long counter;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        counter = Optional.ofNullable(config.getInitParameter("counter"))
+                .map(Long::parseLong)
+                .orElse(0l);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        counter++;
+
+        try(PrintWriter writer = resp.getWriter()){
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta charset='utf-8'>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.printf("<h1>%d</h1>\n",counter);
+            writer.println("</body>");
+            writer.println("</html>");
+        }catch (IOException ex){
+            log.info(ex.getMessage());
+        }
+    }
+}
